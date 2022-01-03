c74005b6f7ef926d123abd116df0eeb5af29923f
stxwright <14978758+stxwright@users.noreply.github.com>
2022-01-03 11:30:39 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const myButton = document.getElementById("myButton");
 myButton.onclick = () => {
-  const h1s = document.getElementsByTagName("h1");
-  const number = Math.floor(Math.random() * 3);
-  switch (number) {
-    case 0:
-      h1s[0].style.color = "green";
-      break;
-    case 1:
-      h1s[0].style.color = "blue";
-      break;
-    case 2:
-      h1s[0].style.color = "red";
-      break;
+  const headerTags = document.querySelectorAll("h1,h2,h3");
+  for (const header of headerTags) {
+    const number = Math.floor(Math.random() * 3);
+    switch (number) {
+      case 0:
+        header.style.color = "green";
+        break;
+      case 1:
+        header.style.color = "blue";
+        break;
+      case 2:
+        header.style.color = "red";
+        break;
+    }
+  }
+  const allSame = Array.from(headerTags).every(
+    (headerTag) => headerTag.style.color === headerTags[0].style.color
+  );
+  if (allSame) {
+    const count = document.getElementById("count");
+    count.innerText = (parseInt(count.textContent) + 1).toString();
   }
 };
