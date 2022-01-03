2c4c508f6fccf9a88605da4ed934ddbdbaefe43d
stxwright <14978758+stxwright@users.noreply.github.com>
2022-01-03 10:48:12 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const myButton = document.getElementById("myButton");
 myButton.onclick = () => {
   const h1s = document.getElementsByTagName("h1");
-  h1s[0].style.color = "green";
+  const number = Math.floor(Math.random() * 3);
+  switch (number) {
+    case 0:
+      h1s[0].style.color = "green";
+      break;
+    case 1:
+      h1s[0].style.color = "blue";
+      break;
+    case 2:
+      h1s[0].style.color = "red";
+      break;
+  }
 };
